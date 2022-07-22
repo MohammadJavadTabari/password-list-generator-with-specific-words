0c7313a22d0f2609a67cfc3f5b698afb9f63c722
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-07-22 08:48:55 -0700

    all words

--- WORDS/PGCW/pgcw.py
+++ WORDS/PGCW/pgcw.py
@@ -1,16 +1,30 @@
 # combinations(p, r) => r-length tuples, in sorted order, no repeated elements
 # this file generate passwords by user words combinations with out replacement
+# => sample:combinations('ABCD', 2) => result: AB AC AD BC BD CD
+#
+# add necessaire library to our code
 import itertools as it
-
+#
+# Getting the possible password words from the user
 words = input("type your words separated with space:")
 word_list = words.split(' ')
-
+#
+# Getting the possible password range from the user
 pass_length = input('What is the range length of the password? example 6-12:')
 pass_length_range = pass_length.split('-')
-
-list_combinations = list()
-
-for i in range(int(pass_length_range[0]), int(pass_length_range[1])):
-    list_combinations += list(it.combinations(word_list, i))
-
-print(list_combinations)
+#
+# open the pass-list file after the loops to avoide of repetitive action => reduce cpu usage
+with open('WORDS\PGCW\pgcw_pass_list.txt', 'w') as f:
+    for i in range(int(pass_length_range[0]), int(pass_length_range[1]) + 1):
+#        
+# get possible passwords by using itter.combinations function 
+        prob_pass_list = list(it.combinations(word_list, i))
+#
+# iterate over each pass-list built by 'combination' function
+        for j in range(len(prob_pass_list)):
+#
+# building our string password by joining probable words in [j] index
+            prob_pass_string = ''.join(prob_pass_list[j])
+#
+# finally should write the password in specific file
+            f.write(f"{prob_pass_string} \n")
--- WORDS/PGPEW/pgpew.py
+++ WORDS/PGPEW/pgpew.py
@@ -1,1 +1,29 @@
 # permutations(p[, r]) => r-length tuples, all possible orderings, no repeated elements
+# => sample: permutations('ABCD', 2) => result: AB AC AD BA BC BD CA CB CD DA DB DC
+#
+# add necessaire library to our code
+import itertools as it
+#
+# Getting the possible password words from the user
+words = input("type your words separated with space:")
+word_list = words.split(' ')
+#
+# Getting the possible password range from the user
+pass_length = input('What is the range length of the password? example 6-12:')
+pass_length_range = pass_length.split('-')
+#
+# open the pass-list file after the loops to avoide of repetitive action => reduce cpu usage
+with open('WORDS\PGPEW\pgpew_pass_list.txt', 'w') as f:
+    for i in range(int(pass_length_range[0]), int(pass_length_range[1]) + 1):
+#        
+# get possible passwords by using itter.permutations function 
+        prob_pass_list = list(it.permutations(word_list, i))
+#
+# iterate over each pass-list built by 'permutations' function
+        for j in range(len(prob_pass_list)):
+#
+# building our string password by joining probable words in [j] index
+            prob_pass_string = ''.join(prob_pass_list[j])
+#
+# finally should write the password in specific file
+            f.write(f"{prob_pass_string} \n")
