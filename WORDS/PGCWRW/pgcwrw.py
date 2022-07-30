78c93f6f55b00d175d4bedf8b534c04aa3f7548e
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-07-30 10:16:32 -0700

    pgcwrw

--- WORDS/PGCWRW/pgcwrw.py
+++ WORDS/PGCWRW/pgcwrw.py
@@ -3,28 +3,9 @@
 # => sample:combinations_with_replacement('ABCD', 2) => result: AA AB AC AD BB BC BD CC CD DD
 #
 # add necessaire library to our code
-import itertools as it
-#
-# Getting the possible password words from the user
-words = input("type your words separated with space:")
-word_list = words.split(' ')
-#
-# Getting the possible password range from the user
-pass_length = input('What is the range length of the password? example 6-12:')
-pass_length_range = pass_length.split('-')
-#
-# open the pass-list file after the loops to avoide of repetitive action => reduce cpu usage
-with open('WORDS\PGCWRW\pgcwrw_pass_list.txt', 'w') as f:
-    for i in range(int(pass_length_range[0]), int(pass_length_range[1]) + 1):
-#        
-# get possible passwords by using itter.combinations_with_replacement function 
-        prob_pass_list = list(it.combinations_with_replacement(word_list, i))
-#
-# iterate over each pass-list built by 'combinations_with_replacement' function
-        for j in range(len(prob_pass_list)):
-#
-# building our string password by joining probable words in [j] index
-            prob_pass_string = ''.join(prob_pass_list[j])
-#
-# finally should write the password in specific file
-            f.write(f"{prob_pass_string} \n")
+import WORDS.main_Services.common_actions_service as cas
+import pgcwrw_service as pgcwrws
+
+word_list = cas.get_words_from_usr()
+start, end = cas.get_pass_range_from_user()
+pgcwrws.combinations_with_replacement_function(word_list=word_list, end_range=end, start_range=start, file_path='WORDS\PGCWRW\pgcwrw_pass_list.txt')
