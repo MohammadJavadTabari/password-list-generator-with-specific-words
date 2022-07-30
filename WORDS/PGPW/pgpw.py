5458714bb7bcb0f64c6a52ba7ffcc2c5e0bf2f63
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-07-30 10:26:16 -0700

    adfncsdlkn

--- WORDS/PGPW/pgpw.py
+++ WORDS/PGPW/pgpw.py
@@ -2,28 +2,9 @@
 # => sample: product('ABCD', repeat=2) => result: AA AB AC AD BA BB BC BD CA CB CC CD DA DB DC DD
 #
 # add necessaire library to our code
-import itertools as it
-#
-# Getting the possible password words from the user
-words = input("type your words separated with space:")
-word_list = words.split(' ')
-#
-# Getting the possible password range from the user
-pass_length = input('What is the range length of the password? example 6-12:')
-pass_length_range = pass_length.split('-')
-#
-# open the pass-list file after the loops to avoide of repetitive action => reduce cpu usage
-with open('WORDS\PGPW\pgpw_pass_list.txt', 'w') as f:
-    for i in range(int(pass_length_range[0]), int(pass_length_range[1]) + 1):
-#        
-# get possible passwords by using itter.product function 
-        prob_pass_list = list(it.product(word_list, i))
-#
-# iterate over each pass-list built by 'product' function
-        for j in range(len(prob_pass_list)):
-#
-# building our string password by joining probable words in [j] index
-            prob_pass_string = ''.join(prob_pass_list[j])
-#
-# finally should write the password in specific file
-            f.write(f"{prob_pass_string} \n")
+import WORDS.main_Services.common_actions_service as cas
+import pgpw_service as pgpws
+
+word_list = cas.get_words_from_usr()
+start, end = cas.get_pass_range_from_user()
+pgpws.product_function()(file_path='WORDS\PGPW\pgpw_pass_list.txt', word_list=word_list, start_range=start, end_range=end)
