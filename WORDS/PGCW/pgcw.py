3c465798428357f9c82cb930f3db2efb1fe80fbd
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-08-02 08:23:25 -0700

    .

--- WORDS/PGCW/pgcw.py
+++ WORDS/PGCW/pgcw.py
@@ -3,11 +3,12 @@
 # # => sample:combinations('ABCD', 2) => result: AB AC AD BC BD CD
 #
 # add necessaire library to our code
-import pgcw_main_service as pgcwms
+import WORDS.main_Services.common_actions_service as cas
+import pgcw_service as pgcwms
 
-word_list = list(pgcwms.get_words_from_usr())
+word_list = list(cas.get_words_from_usr())
 
-start, end = pgcwms.get_pass_range_from_user()
+start, end = cas.get_pass_range_from_user()
 
 pgcwms.combinations_functions(file_path='WORDS\PGCW\pgcw_pass_list.txt',
  start_range=start, end_range=end, word_list=word_list)
